dc0e88a10f0e231021615490b5c67fb0381c5049
Timothe <hegetimothe@gmail.com>
2024-04-06 19:40:48 +0200

    ajout de l'hero avec le dark et animation pour le responsiv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import path from 'path';
 import { NavComponent } from './components/sections/nav/nav.component';
+import { HeroComponent } from './components/sections/hero/hero.component';
 
 const routes: Routes = [
   {path: 'nav-bar', component: NavComponent},
+  {path: '', component: HeroComponent}
 ];
 
 @NgModule({
