8bd8c777fa80612bc19b3fe724fe912a2feacec4
Timothe <hegetimothe@gmail.com>
2024-04-07 18:51:04 +0200

    scroll

--- src/app/components/modules/infinite-scrolling-text/infinite-scrolling-text.component.ts
+++ src/app/components/modules/infinite-scrolling-text/infinite-scrolling-text.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-infinite-scrolling-text',
-  templateUrl: './infinite-scrolling-text.component.html',
-  styleUrl: './infinite-scrolling-text.component.scss'
+  template: `
+    <div class="container">
+      <span class="txt t1">{{ text1 }}</span>
+      <span class="txt t2">{{ text2 }}</span>  
+    </div>
+  `,
+  styleUrls: ['./infinite-scrolling-text.component.scss']
 })
 export class InfiniteScrollingTextComponent {
-
+  @Input() text1!: string;
+  @Input() text2!: string;
 }
